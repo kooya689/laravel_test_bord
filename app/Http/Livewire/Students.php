bee55a2caec4f3d7024484162db732a524963a60
sugar <sugar@example.com>
2021-06-17 12:15:19 +0900

    編集と削除の機能

--- app/Http/Livewire/Students.php
+++ app/Http/Livewire/Students.php
@@ -14,6 +14,7 @@ class Students extends Component
     public $email;
     public $phone;
     public $modalStatus;
+    public $modalUpdateStatus;
 
     public function render()
     {
@@ -21,6 +22,8 @@ class Students extends Component
         return view('livewire.students.index', compact('students')); //追加
     }
 
+
+    //新規投稿機能
     
         public function openModal() //modalStatusをtrueにしmodalを表示する
         {
@@ -56,6 +59,59 @@ class Students extends Component
             session()->flash('message', '新規投稿に成功しました。');
             $this->resetInputFields();
             $this->closeModal();
+        }
+
+        //編集機能
+        
+
+        public function openUpdateModal($id)
+        {
+            $student = Student::where('id', $id)->first();
+            $this->ids = $student->id;
+            $this->firstname = $student->firstname;
+            $this->lastname = $student->lastname;
+            $this->email = $student->email;
+            $this->phone = $student->phone;
+            $this->modalUpdateStatus = true;
+            
+        }
+
+        public function closeUpdateModal()
+        {
+            $this->modalUpdateStatus = false;
+        }
+
+        public function update()
+        {
+            $validateData = $this->validate([
+                'firstname' => 'required',
+                'lastname' => 'required',
+                'email' => 'required|email',
+                'phone' => 'required',                
+            ]);
+            if ($this->ids) {
+                $student = Student::find($this->ids);
+                $student->update([
+                    'firstname' => $this->firstname,
+                    'lastname' => $this->lastname,
+                    'email' => $this->email,
+                    'phone' => $this->phone,
+            ]);
+            session()->flash('message', '投稿の編集に成功しました。');
+            $this->resetInputFields();
+            $this->closeUpdateModal();
+            }
+        }
+
+        //削除機能
+
+        public function delete($id)
+        {
+            if($id)
+            {
+                Student::where('id', $id)->delete();
+                session()->flash('message', '投稿の削除に成功しました。');
+            }
         }
 
 }
